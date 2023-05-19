da17d41d17009da0a53577004156e5c43e3563a8
pifou25 <nicolas_gras@hotmail.com>
2023-05-19 18:22:28 +0200

    feat: add index configuration export

---
+++ index.js
@@ -0,0 +1,53 @@
+/**
+ * Semantic Release shareable config
+ * for Jeedom Beta
+ * with changelog genetaror, pluginVersion replacement
+ */
+module.exports = {
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+    [
+      "@semantic-release/changelog",
+      {
+        "changelogFile": "docs/fr_FR/changelog_beta.md"
+      }
+    ],
+    [
+      "@semantic-release/npm",
+      {
+        "npmPublish": false
+      }
+    ],
+    "@semantic-release/github",
+    [
+      "@google/semantic-release-replace-plugin",
+      {
+        replacements: [
+          {
+            files: ["plugin_info/info.json"],
+            from: '"pluginVersion": ".*",',
+            to: '"pluginVersion": "${nextRelease.version}",',
+            results: [
+              {
+                file: "plugin_info/info.json",
+                hasChanged: true,
+                numMatches: 1,
+                numReplacements: 1,
+              },
+            ],
+            countMatches: true,
+          },
+        ],
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        message: `chore(release): \${nextRelease.version} [skip ci]`,
+        assets: ["plugin_info/info.json", "docs/fr_FR/changelog_beta.md"],
+      },
+    ],
+  ],
+};
